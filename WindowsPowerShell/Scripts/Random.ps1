f7e15b54edc42f07e45b63aabbf1563a745f66bf
Dustin Venegas <dustin.venegas@gmail.com>
2015-07-06 17:05:05 -0700

    Random Functions
    Here, let's throw all these here for now. It will eventually grow to a
    point of annoyance where separating it out actually makes sense.

---
+++ WindowsPowerShell/Scripts/Random.ps1
@@ -0,0 +1,45 @@
+function Set-IisPremissions {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$directory)
+
+    if (-not (Test-Path $directory)) {
+        throw "Can't set IIS Premissions on missing directory $directory"
+    }
+
+    $acl = Get-Acl $directory
+
+    $inherit = [system.security.accesscontrol.InheritanceFlags]"ContainerInherit, ObjectInherit"
+    $propagation = [system.security.accesscontrol.PropagationFlags]"None"
+ 
+    $permission = 'IIS_IUSRS',"FullControl", $inherit, $propagation, "Allow"
+    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+    $acl.SetAccessRule($accessRule)
+    
+    $permission = 'NETWORK SERVICE',"FullControl", $inherit, $propagation, "Allow"
+    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+    $acl.SetAccessRule($accessRule)
+       
+    
+    $acl | Set-Acl $directory
+}
+
+function Test-Port {
+    param (
+        [string]$Computer, 
+        [int]$Port)
+
+    $Socket = New-Object Net.Sockets.TcpClient
+    try {
+        $Socket.Connect($Computer, $Port)
+    } catch {}
+
+    if ($Socket.Connected) {
+        $true
+        $Socket.Close()
+    } else {
+        $false
+    }
+
+    $Socket = $null
+}
