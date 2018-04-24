f3069f7c6f93f2c75aa716cc34ed764263fa562f
Dustin Venegas <dustin.venegas@gmail.com>
2018-04-24 00:09:40 -0700

    PowerShell, move host related color functions into host-only config

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -15,6 +15,58 @@ function Write-MessageOfTheDay
     Write-Output "I'd like to see you move up to the emu class, where I think you belong."
 }
 
+function Set-PowerShellHostCustomColor
+{
+    # Distinguish different types of pipeline messages using custom colors.
+    # Helps differentiate between warning/verbose which are both "Yellow" by default
+
+    # Get a reference to the host's color data
+    $hostPrivateDataOpts = (Get-Host).PrivateData
+
+    # Green on black; pretty much hidden
+    $hostPrivateDataOpts.DebugForegroundColor = "Green"
+    $hostPrivateDataOpts.DebugBackgroundColor = "Black"
+
+    # Black on dark gray; brighter than debug
+    $hostPrivateDataOpts.VerboseForegroundColor = "Black"
+    $hostPrivateDataOpts.VerboseBackgroundColor = "DarkGray"
+
+    # White on yellow;
+    $hostPrivateDataOpts.WarningForegroundColor = "DarkGray"
+    $hostPrivateDataOpts.WarningBackgroundColor = "Yellow"
+
+    $hostPrivateDataOpts.ErrorForegroundColor = "White"
+    $hostPrivateDataOpts.ErrorBackgroundColor = "Red"
+}
+
+function Write-StreamColorVariations
+{
+    # Useful for verifying colors. Writes examples of various textual or logging pipelines.
+
+    # Momento global preferences to work around `-debug` switch starting PS Debugger
+    $origDebugPreference = $DebugPreference
+    $DebugPreference = 'Continue'
+
+    # Write examples on each textual pipeline
+    @('Debug','Verbose','Information','Warning','Error','Host') |
+        %{ &"Write-$($_)" $($_) -InformationAction Continue -WarningAction Continue -ErrorAction Continue -Verbose}
+
+    # Reset preferences to momento value
+    $DebugPreference = $origDebugPreference
+
+    # Write some related preferences using Write-Information with
+    Write-Information @"
+Related Preferences
+  `$DebugPreference = $DebugPreference
+  `$VerbosePreference = $VerbosePreference
+  `$InformationPreference = $InformationPreference
+  `$WarningPreference = $WarningPreference
+  `$ErrorActionPreference = $ErrorActionPreference
+  `$ProgressPreference = $ProgressPreference
+"@ -InformationAction Continue
+}
+
 Write-MessageOfTheDay
+Set-PowerShellHostCustomColor
 
 Set-StrictMode -Off
