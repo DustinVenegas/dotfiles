1db4167dfb4a0e6c2ddf95cdcc703a1cbc9a9256
Dustin Venegas <dustin.venegas@gmail.com>
2019-04-21 20:55:16 -0700

    Ripgrep on Linux

---
+++ ripgrep/bootstrap.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+symlink_if_missing()
+{
+    SOURCE=$1
+    DEST=$2
+
+    if [ ! -f "$DEST" ]; then
+        ln -s "$SOURCE" "$DEST"
+    else
+	echo "Already exists $SOURCE -> $DEST"
+    fi
+}
+
+write_template_if_missing()
+{
+    SOURCE=$1
+    DEST=$2
+
+    if [ ! -f "$DEST" ]; then
+        cp "$SOURCE" "$DEST"
+    else
+	echo "Already exists $SOURCE -> $DEST"
+    fi
+}
+
+
+# TODO: Support XDG_CONFIG_HOME
+SCRIPT_DIR="$(cd "$(dirname "${0}")"; pwd)"
+
+# Symlink configurations, if they don't already exist
+symlink_if_missing "$SCRIPT_DIR/ripgreprc" "$HOME/.ripgreprc"
+
+# OF-Specific Smybolic Links
+OS="`uname`"
+case $OS in
+    'Linux')
+	sed 's/#.*//' ubuntu-installs.txt | xargs sudo apt-get install
+        ;;
+    'Darwin')
+        ;;
+    *)
+        echo "WARNNG: gitconfig_os type is missing, $OSTYPE"
+        ;;
+esac
