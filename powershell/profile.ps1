bfae1d224e66935b799f6b9dda3ee7448274d078
Dustin Venegas <dustin.venegas@gmail.com>
2020-03-09 12:49:49 -0700

    Style

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,9 +1,9 @@
 $env:EDITOR = 'nvim'
-$MaximumHistoryCount=10000
+$MaximumHistoryCount = 10000
 
 function promptForPOSHGit {
     # colorStatus: Green if no errors, red if errors
-    $colorStatus = if ($? -eq $true) {[ConsoleColor]::DarkCyan} else { [ConsoleColor]::Red }
+    $colorStatus = if ($? -eq $true) { [ConsoleColor]::DarkCyan } else { [ConsoleColor]::Red }
 
     # Your non-prompt logic here
     $prompt = Write-Prompt "$([System.Environment]::NewLine)$([char]0x0A7) " -ForegroundColor $colorStatus
@@ -20,16 +20,16 @@ function promptForPOSHGit {
     $prompt += Write-Prompt "$([System.Environment]::NewLine)"
     $prompt += Write-Prompt "$([DateTime]::now.ToString("HH:mm:ss")) " -ForegroundColor Blue
     $prompt += Write-Prompt "$((Get-History -Count 1).id + 1)$('>' * ($nestedPromptLevel))" -ForegroundColor DarkGray
-    if ($prompt) { $prompt  } else { " " }
+    if ($prompt) { $prompt } else { " " }
 }
 
 function Prompt {
     $prompt = promptForPOSHGit
-    if ($prompt) { $prompt  } else { " " }
+    if ($prompt) { $prompt } else { " " }
 }
 
 if (Test-Path "$HOME/.ripgreprc") {
-    $env:RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
+    $env:RIPGREP_CONFIG_PATH = "$HOME/.ripgreprc"
 }
 
 if (Test-Path "$PSScriptRoot/profile.local.ps1") {
