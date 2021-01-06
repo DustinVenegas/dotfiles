b5b4d9f1a089dd7abc70b7f769645efa16efc26a
Dustin Venegas <dustin.venegas@gmail.com>
2021-01-05 17:48:01 -0800

    vim, a generic entrypoint for vimrc in $HOME
    _vimrc on Windows
    .vimrc on Unix-based OSes

--- vim/bootstrap.ps1
+++ vim/bootstrap.ps1
@@ -9,16 +9,17 @@ param()
 begin {
     Import-Module -Name (Resolve-Path (Join-Path $PSScriptRoot ../powershell-modules/Dotfiles/Dotfiles.psm1))
     Set-StrictMode -Version latest
+
+    $vimrcFilename = '.vimrc'
+    if (Test-OSPlatform -Include 'Windows') {
+        $vimrcFilename = '_vimrc'
+    }
 }
 process {
     Install-Packages $PSScriptRoot
 
     New-SymbolicLink -Path $(Join-Path -Path $HOME -ChildPath '.vim') -Value $PSScriptRoot
-
-    if (Test-OSPlatform -Include 'Windows') {
-        # Link .vimrc.windows to the default vim config on Windows.
-        New-SymbolicLink -Path $(Join-Path $HOME '_vimrc') -Value $(Join-Path $PSScriptRoot '.vimrc.windows')
-    }
+    New-SymbolicLink -Path $(Join-Path -Path $HOME -ChildPath $vimrcFileName) -Value (Join-Path $PSScriptRoot '.vimrc.entrypoint')
 
     Invoke-Everytime -Name 'Update-VimPlugins' -ScriptBlock {
         # Blindly update plugins in vim.
