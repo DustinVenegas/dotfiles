ed74a2ce625472b20da84b47926f4ad1017bfa7e
Dustin Venegas <dustin.venegas@gmail.com>
2021-01-17 00:33:30 -0800

    vscode, switch to built-in settings sync

--- VSCode/bootstrap.ps1
+++ VSCode/bootstrap.ps1
@@ -8,25 +8,7 @@
 param()
 begin {
     Import-Module -Name (Resolve-Path (Join-Path $PSScriptRoot ../powershell-modules/Dotfiles/Dotfiles.psm1))
-
-    $extensionsToManage = @(
-        'EditorConfig.EditorConfig',
-        'vscodevim.vim',
-        'Shan.code-settings-sync'
-    )
-
-    $installedExtensions = code --list-extensions
-    $extensionsToInstall = $extensionsToManage | Where-Object {
-        $_ -NotIn $installedExtensions
-    }
 }
 process {
     Install-Packages $PSScriptRoot
-
-    # Install VSCode Extensions
-    foreach ($name in $extensionsToInstall) {
-        Invoke-Everytime -Name "Install-VSCodeExtension-$name" -ScriptBlock {
-            code --install-extension $name
-        }
-    }
 }
