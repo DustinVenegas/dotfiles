83c8ebab4945ba2fac3e955dcc870fdf06a39e99
Dustin Venegas <dustin.venegas@gmail.com>
2022-07-16 09:40:21 -0700

    powershell, enables dotnet completion

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -126,3 +126,18 @@ if (Get-Module -Name ZLocation -ListAvailable -ErrorAction SilentlyContinue) {
     $zlocationEnabled = $true
 }
 
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
+if (Get-Command 'dotnet' -ErrorAction SilentlyContinue) {
+    # PowerShell parameter completion shim for the dotnet CLI
+    Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+        param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+    }
+}
